024a93b00da6227d2e194e338c8437298ef83954
TANVIR0222 <tanvirislam3912@gmail.com>
2026-06-18 23:07:36 +0600

    feat: enhance todoAdd function with error handling and logging; update dashboard page to log DATABASE_URL

--- actions/todoAction.ts
+++ actions/todoAction.ts
@@ -1,12 +1,18 @@
+"use server";
+
+import { db } from "@/db/drizzle";
+import { todo } from "@/migrations/schema";
 import { todoType } from "@/types/todoType";
 
 export default async function todoAdd(data: todoType) {
   console.log(data);
   console.log("---------", process?.env?.DATABASE_URL);
 
-  // try {
-  //   const res = await db.insert(todo).values(data);
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  try {
+    const result = await db.insert(todo).values(data);
+    if (result?.rowCount) console.log(result);
+    // alert("success");
+  } catch (err) {
+    console.log(err);
+  }
 }

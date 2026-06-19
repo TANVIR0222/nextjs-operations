bdf0069fe3dd548c8c9e108b3705794bcf209efa
TANVIR0222 <tanvirislam3912@gmail.com>
2026-06-19 23:30:23 +0600

    user registration ui

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegistrationForm } from "@/feature/auth/components/registration-form";
+
+const page = () => {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +26,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-full bg-black flex flex-col antialiased",
+          geistSans.variable,
+          geistMono.variable,
+          inter.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
